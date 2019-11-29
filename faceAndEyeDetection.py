2c09afa381cdebe65959274f70cfa0becc163df4
Thomazeau Louis <louis.thomazeau@student-cs.fr>
2019-11-29 10:47:18 +0100

    Change eye rectangles color

--- faceAndEyeDetection.py
+++ faceAndEyeDetection.py
@@ -17,7 +17,7 @@ while 1:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         
