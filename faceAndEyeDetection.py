f23cf67c68bf5630f992bdcfe9dffa47f9bd81b8
Alexandre Burrus <alexandre.burrus@student-cs.fr>
2019-12-06 20:44:25 +0100

    Detection tête et mouvement yeux

--- faceAndEyeDetection.py
+++ faceAndEyeDetection.py
@@ -8,12 +8,14 @@ face_cascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.x
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("test_video.mov")
 
 while 1:
     ret, img = cap.read()
-    img = cv2.flip(img,1)
+    img = cv2.flip(img,0)
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
@@ -22,8 +24,15 @@ while 1:
         roi_color = img[y:y+h, x:x+w]
         
         eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
+        eyes_sorted=sorted(eyes, key= lambda x:x[3]*x[2],reverse=True)
+
+        i=0
+        for (ex,ey,ew,eh) in eyes_sorted:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            i+=1
+            if i>=2:
+                break
+
 
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
