cc6178d2519c6a4f0f28a487fd3e5ed9dd4c5d24
Burrus Alexandre <alexandre.burrus@student-cs.fr>
2020-02-10 09:44:46 +0100

    mise en forme

--- eye_motion_tracking.py
+++ eye_motion_tracking.py
@@ -1,39 +1,39 @@
 import cv2
 import numpy as np
 
-#source de la vidéo
+# source de la vidéo
 cap = cv2.VideoCapture("test_video.MOV")
 
 while 1:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 0)
 
-    #définit la région d'intérêt pour l'oeil droit et gauche
-    X_d,Y_d,W_d,H_d=1000,360,170,130
-    X_g,Y_g,W_g,H_g=800,360,170,130
-    
+    # définit la région d'intérêt pour l'oeil droit et gauche
+    X_d, Y_d, W_d, H_d = 1000, 360, 170, 130
+    X_g, Y_g, W_g, H_g = 800, 360, 170, 130
+
     roi_droite = frame[Y_d: Y_d+H_d, X_d: X_d+W_d]
     roi_gauche = frame[Y_g: Y_g+H_g, X_g: X_g+W_g]
 
     cv2.rectangle(frame, (X_d, Y_d), (X_d + W_d, Y_d + H_d), (255, 0, 0), 2)
     cv2.rectangle(frame, (X_g, Y_g), (X_g + W_g, Y_g + H_g), (255, 0, 0), 2)
-               
-    #cv2.imshow("roi_gauche",roi_gauche)
 
-    #calcule la taille en pixels de ces cadrages
+    # cv2.imshow("roi_gauche",roi_gauche)
+
+    # calcule la taille en pixels de ces cadrages
     rows_droite, cols_droite, _ = roi_droite.shape
     rows_gauche, cols_gauche, _ = roi_gauche.shape
-    
-    #convertit l'image en nuances de gris
+
+    # convertit l'image en nuances de gris
     gray_roi_droite = cv2.cvtColor(roi_droite, cv2.COLOR_BGR2GRAY)
     gray_roi_gauche = cv2.cvtColor(roi_gauche, cv2.COLOR_BGR2GRAY)
 
-
-
-    #floute l'image obtenue afin de réduire le bruit
-    blur=7 #degré du floutage (mettre nombre impair)
-    gray_roi_droite_blurred = cv2.GaussianBlur(gray_roi_droite, (blur,blur), 7)
-    gray_roi_gauche_blurred = cv2.GaussianBlur(gray_roi_gauche, (blur, blur), 7)
+    # floute l'image obtenue afin de réduire le bruit
+    blur = 7  # degré du floutage (mettre nombre impair)
+    gray_roi_droite_blurred = cv2.GaussianBlur(
+        gray_roi_droite, (blur, blur), 7)
+    gray_roi_gauche_blurred = cv2.GaussianBlur(
+        gray_roi_gauche, (blur, blur), 7)
 
     """
     #affiche les nouvelles séquences
@@ -41,9 +41,11 @@ while 1:
     cv2.imshow("gray_roi_gauche_blurred",gray_roi_gauche_blurred)
     """
 
-    #sépare la séquence en noir et blanc suivant une certaine limite de nuance de gris
-    _, threshold_droite = cv2.threshold(gray_roi_droite_blurred, 3, 255, cv2.THRESH_BINARY_INV)
-    _, threshold_gauche = cv2.threshold(gray_roi_gauche_blurred, 3, 255, cv2.THRESH_BINARY_INV)
+    # sépare la séquence en noir et blanc suivant une certaine limite de nuance de gris
+    _, threshold_droite = cv2.threshold(
+        gray_roi_droite_blurred, 3, 255, cv2.THRESH_BINARY_INV)
+    _, threshold_gauche = cv2.threshold(
+        gray_roi_gauche_blurred, 3, 255, cv2.THRESH_BINARY_INV)
 
     """
     #affiche les nouvelles séquences
@@ -51,49 +53,56 @@ while 1:
     cv2.imshow("threshold_gauche",threshold_gauche)
     """
 
-    #renvoie des listes des coordonnées des contours des zones blanches
-    contours_droite, _ = cv2.findContours( threshold_droite, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours_gauche, _ = cv2.findContours( threshold_gauche, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-    #trie ses listes afin d'avoir le plus grand contour en premier élément
-    contours_droite_sorted = sorted(contours_droite, key=lambda x: cv2.contourArea(x), reverse=True)
-    contours_gauche_sorted = sorted(contours_gauche, key=lambda x: cv2.contourArea(x), reverse=True)
+    # renvoie des listes des coordonnées des contours des zones blanches
+    contours_droite, _ = cv2.findContours(
+        threshold_droite, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_gauche, _ = cv2.findContours(
+        threshold_gauche, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    #si un contour a été détecté, on renvoie la liste du plus grand contour
-    if not len(contours_droite_sorted)==0:
+    # trie ses listes afin d'avoir le plus grand contour en premier élément
+    contours_droite_sorted = sorted(
+        contours_droite, key=lambda x: cv2.contourArea(x), reverse=True)
+    contours_gauche_sorted = sorted(
+        contours_gauche, key=lambda x: cv2.contourArea(x), reverse=True)
 
-        cnt_d=contours_droite_sorted[0]
+    # si un contour a été détecté, on renvoie la liste du plus grand contour
+    if not len(contours_droite_sorted) == 0:
+
+        cnt_d = contours_droite_sorted[0]
 
         (x_d, y_d, w_d, h_d) = cv2.boundingRect(cnt_d)
 
         #cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 3)
         #cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        cv2.line(roi_droite, (x_d + int(w_d/2), 0), (x_d + int(w_d/2), rows_droite), (0, 255, 0), 2)
-        cv2.line(roi_droite, (0, y_d + int(h_d/2)), (cols_droite, y_d + int(h_d/2)), (0, 255, 0), 2)
-   
-    #si un contour a été détecté, on renvoie la liste du plus grand contour
-    if not len(contours_gauche_sorted)==0:
+        cv2.line(roi_droite, (x_d + int(w_d/2), 0),
+                 (x_d + int(w_d/2), rows_droite), (0, 255, 0), 2)
+        cv2.line(roi_droite, (0, y_d + int(h_d/2)),
+                 (cols_droite, y_d + int(h_d/2)), (0, 255, 0), 2)
 
-        cnt_g=contours_gauche_sorted[0]
+    # si un contour a été détecté, on renvoie la liste du plus grand contour
+    if not len(contours_gauche_sorted) == 0:
+
+        cnt_g = contours_gauche_sorted[0]
 
         (x_g, y_g, w_g, h_g) = cv2.boundingRect(cnt_g)
-        
+
         #cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 3)
         #cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        
-        cv2.line(roi_gauche, (x_g + int(w_g/2), 0), (x_g + int(w_g/2), rows_gauche), (0, 255, 0), 2)
-        cv2.line(roi_gauche, (0, y_g + int(h_g/2)), (cols_gauche, y_g + int(h_g/2)), (0, 255, 0), 2)
 
+        cv2.line(roi_gauche, (x_g + int(w_g/2), 0),
+                 (x_g + int(w_g/2), rows_gauche), (0, 255, 0), 2)
+        cv2.line(roi_gauche, (0, y_g + int(h_g/2)),
+                 (cols_gauche, y_g + int(h_g/2)), (0, 255, 0), 2)
 
-    #cv2.imshow('roi_droite',roi_droite)
-    #cv2.imshow('roi_gauche',roi_gauche)
+    # cv2.imshow('roi_droite',roi_droite)
+    # cv2.imshow('roi_gauche',roi_gauche)
 
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
-       break
-            
+        break
+
 cap.release()
 cv2.destroyAllWindows()
