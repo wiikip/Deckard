60fde42e930b049920a7fe6e84d59625309cc2f6
Alexandre <alexandre.burrus@student-cs.fr>
2020-02-10 10:42:26 +0100

    traduction code

--- faceDetectionAndEyeGaze_retina.py
+++ faceDetectionAndEyeGaze_retina.py
@@ -2,16 +2,16 @@ import cv2
 import numpy as np
 
 # https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
-face_cascade = cv2.CascadeClassifier(
+faceCascade = cv2.CascadeClassifier(
     'Cascades/haarcascade_frontalface_default.xml')
 # https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
-eye_cascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
+eyeCascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
 
 # source de la vidéo
 #cap = cv2.VideoCapture("test_video.MOV")
 cap = cv2.VideoCapture(0)
 
-oeil1_me_regarde, oeil2_me_regarde = False, False
+firstEyeGazes, secondEyeGazes = False, False
 
 
 while 1:
@@ -19,68 +19,69 @@ while 1:
     frame = cv2.flip(frame, 1)
 
     # je considère que la personne regarde la caméra lorsque un des 2 yeux la regarde
-    la_personne_me_regarde = oeil1_me_regarde and oeil2_me_regarde
+    personGazes = firstEyeGazes or secondEyeGazes
 
     # convertit l'image en 50 nuances de gris
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # detecte les têtes
-    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+    faces = faceCascade.detectMultiScale(grayFrame, 1.3, 5)
     # trier les têtes
     if len(faces) != 0:
 
         (x, y, w, h) = faces[0]
 
-        if la_personne_me_regarde:
+        if personGazes:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
         else:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
 
-        # oeil1_me_regarde,oeil2_me_regarde=False,False
+        firstEyeGazes, secondEyeGazes = False, False
 
-        gray_roi = gray_frame[y:y+h, x:x+w]
-        color_roi = frame[y:y+h, x:x+w]
+        grayRoi = grayFrame[y:y+h, x:x+w]
+        colorRoi = frame[y:y+h, x:x+w]
 
-        eyes = eye_cascade.detectMultiScale(gray_roi)
-        eyes_sorted = sorted(eyes, key=lambda x: x[3]*x[2], reverse=True)
+        eyes = eyeCascade.detectMultiScale(grayRoi)
 
-        compteur_yeux = 0
+        eyesSorted = sorted(eyes, key=lambda x: x[3]*x[2], reverse=True)
 
-        for (ex, ey, ew, eh) in eyes_sorted:
+        eyeCounter = 0
 
-            cv2.rectangle(color_roi, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
+        for (ex, ey, ew, eh) in eyesSorted:
+
+            cv2.rectangle(colorRoi, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
 
             # définit la région d'intérêt pour un oeil
-            eye_gray_roi = gray_roi[ey: ey+eh, ex:ex+ew]
-            eye_color_roi = color_roi[ey:ey+eh, ex:ex+ew]
+            eyeGrayRoi = grayRoi[ey: ey+eh, ex:ex+ew]
+            eyeColorRoi = colorRoi[ey:ey+eh, ex:ex+ew]
 
             # calcule la taille en pixels de ces cadrages
-            rows, cols = eye_gray_roi.shape
+            rows, cols = eyeGrayRoi.shape
 
             # floute l'image obtenue afin de réduire le bruit
             blur = 7  # degré du floutage (mettre nombre impair)
-            blurred_blurred = cv2.GaussianBlur(eye_gray_roi, (blur, blur), 7)
+            blurred_blurred = cv2.GaussianBlur(eyeGrayRoi, (blur, blur), 7)
 
             # sépare la séquence en noir et blanc suivant une certaine limite de nuance de gris
             _, threshold = cv2.threshold(
-                eye_gray_roi, 35, 255, cv2.THRESH_BINARY_INV)
+                eyeGrayRoi, 35, 255, cv2.THRESH_BINARY_INV)
 
-            # renvoie des listes des coordonnées des contours des zones blanches
-            contours, _ = cv2.findContours(
+            # renvoie des listes des coordonnées des outline des zones blanches
+            outline, _ = cv2.findContours(
                 threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
             # trie ses listes afin d'avoir le plus grand contour en premier élément
-            contours_sorted = sorted(
-                contours, key=lambda x: cv2.contourArea(x), reverse=True)
+            outlineSorted = sorted(
+                outline, key=lambda x: cv2.contourArea(x), reverse=True)
 
             # si un contour a été détecté, on renvoie la liste du plus grand contour
-            if len(contours_sorted) != 0:
+            if len(outlineSorted) != 0:
 
-                cnt = contours_sorted[0]
+                outlineBis = outlineSorted[0]
 
-                (cx, cy, cw, ch) = cv2.boundingRect(cnt)
-                cv2.rectangle(eye_color_roi, (cx, cy),
+                (cx, cy, cw, ch) = cv2.boundingRect(outlineBis)
+                cv2.rectangle(eyeColorRoi, (cx, cy),
                               (cx+cw, cy+ch), (255, 0, 0), 2)
 
                 abscisse_du_centre = cx+int(cw/2)
@@ -89,17 +90,17 @@ while 1:
                 pas_y = int(eh/50)
 
                 # problème de coordonnées
-                if compteur_yeux == 0:
+                if eyeCounter == 0:
 
-                    oeil1_me_regarde = (24*pas_x < abscisse_du_centre < ew-24 *
-                                        pas_x) and (24*pas_y < ordonnee_du_centre < eh-24*pas_y)
+                    firstEyeGazes = (24*pas_x < abscisse_du_centre < ew-24 *
+                                     pas_x) and (24*pas_y < ordonnee_du_centre < eh-24*pas_y)
 
                 else:
-                    oeil2_me_regarde = (24*pas_x < abscisse_du_centre < ew-24 *
-                                        pas_x) and (24*pas_y < ordonnee_du_centre < eh-24*pas_y)
+                    secondEyeGazes = (24*pas_x < abscisse_du_centre < ew-24 *
+                                      pas_x) and (24*pas_y < ordonnee_du_centre < eh-24*pas_y)
 
-            compteur_yeux += 1
-            if compteur_yeux >= 2:
+            eyeCounter += 1
+            if eyeCounter >= 2:
                 break
 
         cv2.imshow("frame", frame)
